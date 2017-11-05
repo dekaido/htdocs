d95e1ae6a4828f30809a268d194c255c867e7869
dekaido <gml5197@psu.edu>
2017-11-05 12:51:07 -0500

    User form will update user to database.

--- user.php
+++ user.php
@@ -20,17 +20,15 @@ if(mysqli_connect_error())
 {
 	die("Connection failed: " . mysqli_connect_error());	
 }
-#$cmd1 = $mysqli->prepare("SELECT * FROM starfish WHERE id = ?");
-#$cmd1->bind_param('d',$id);
-#$cmd1->execute();
+
 $cmd1 ="SELECT major,gpa,credits FROM starfish WHERE id = $id ORDER BY credits DESC;";
+$cmd2 = "INSERT INTO user (id,firstName,lastName) VALUES ('$id','$first','$last')";
+
 $res1 = mysqli_query($connect,$cmd1);
 
-#echo mysqli_num_rows($res1), " entries found.<br>";
 if(mysqli_num_rows($res1) > 0)
 {
 	$row = mysqli_fetch_assoc($res1);
-	#vprintf("ID: %7d ",array($row["id"]));
 	$sem = mysqli_num_rows($res1)/2;
 	echo "Semester: " . ceil($sem) . " ";# . "<br>";
 	vprintf("Major: %s  ",array($row["major"]));
@@ -38,24 +36,14 @@ if(mysqli_num_rows($res1) > 0)
 	vprintf("Credits: %.2f  ",array($row["credits"]));
 	echo "<br><br><br>";
 	echo "*GPA of prior semester.<br>**Credits accumulated.<br>";
-	/*while($row = mysqli_fetch_assoc($res1))
-	{		
-		vprintf("ID: %7d ",array($row["id"]));
-		#echo "\t";
-		vprintf("MAJOR: %s  ",array($row["major"]));
-		#echo "\t\t\t";
-		vprintf("GPA: %.2f  ",array($row["gpa"]));
-		#echo "\t";
-		vprintf("CREDITS: %.2f  ",array($row["credits"]));
-		echo "<br>";
-	
-	}	
-	*/
 }
 else
 {
 	echo "0 results.";
 }
+
+$res2 = mysqli_query($connect,$cmd2);
+if(!$res2) echo mysqli_error($connect);
 
 mysqli_close($connect);
 
