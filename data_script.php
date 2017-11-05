b9d24e334b4dc1cb3e4d19bd92578dd9528487e5
dekaido <gml5197@psu.edu>
2017-11-05 10:00:48 -0500

    Database entries can be refreshed and prints out to page.

--- data_script.php
+++ data_script.php
@@ -10,9 +10,10 @@ if(mysqli_connect_error())
 {
 	die("Connection failed: " . mysqli_connect_error());	
 }
-echo "Connection successful";
+echo "Connection successful. <br>";
 
-$cmd1 = "LOAD DATA INFILE 'StarfishStudentData.csv' 
+$cmd1 = "DELETE FROM starfish;";
+$cmd2 = "LOAD DATA INFILE 'StarfishStudentData.csv' 
 		REPLACE INTO TABLE starfish
 		FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
 		IGNORE 1 LINES
@@ -22,19 +23,45 @@ $cmd1 = "LOAD DATA INFILE 'StarfishStudentData.csv'
 		@dummy,@dummy,@gpa) 
 		set id=@id,major=@major,gpa=@gpa,credits=@credits;";
 		
-$cmd2 = "LOAD DATA INFILE 'StarfishStudentData.csv' INTO TABLE starfish
+$cmd3 = "LOAD DATA INFILE 'StarfishStudentData.csv' INTO TABLE starfish
 		FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
 		(@person_party_id,@prior_total_credit_hours,@primary_major,
 		@prior_term_gpa) set id=@person_party_id,major=@primary_major,
 		gpa=@prior_term_gpa,credits=@prior_total_credit_hours;";
 		
-$cmd3 = "SELECT * FROM starfish;";
+$cmd4 = "SELECT * FROM starfish;";
 
 $mysqli = mysqli_query($connect,$cmd1);
 if(!$mysqli) echo mysqli_error($connect);
 
-$mysqli = mysqli_query($connect,$cmd3);
+$mysqli = mysqli_query($connect,$cmd2);
 if(!$mysqli) echo mysqli_error($connect);
+
+#$result4 = $cmd4->query($cmd4);
+$result4 = mysqli_query($connect, $cmd4);
+echo mysqli_num_rows($result4), " entries updated.<br>";
+if(mysqli_num_rows($result4) > 0)
+{
+	while($row = mysqli_fetch_assoc($result4))
+	{
+		
+		vprintf("ID: %7d ",array($row["id"]));
+		#echo "\t";
+		vprintf("MAJOR: %s  ",array($row["major"]));
+		#echo "\t\t\t";
+		vprintf("GPA: %.2f  ",array($row["gpa"]));
+		#echo "\t";
+		vprintf("CREDITS: %.2f  ",array($row["credits"]));
+		echo "<br>";
+		
+		#echo "ID: " . $row["id"] . "Major: " . $row["major"] . 
+		#"GPA: " . $row["gpa"] . "Credits: " . $row["credits"] . "<br>";
+	}	
+}
+else
+{
+	echo "0 results.";
+}
 
 mysqli_close($connect);
 ?>
